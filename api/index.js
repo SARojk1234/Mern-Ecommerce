8e0ffbdce39e4be382bb440858f3bda7feacf870
SARojk1234 <sarojking720@gmail.com>
2024-03-22 12:12:03 +0530

    test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv'
+import userRoutes from './route/user.route.js'
 dotenv.config();
 
 
@@ -16,3 +17,5 @@ mongoose.connect(process.env.MONGO).then(()=>{
 .catch((err)=>{
     console.log(err);
 })
+
+app.use('/api/user', userRoutes);
