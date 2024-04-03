3d20993ff462f663c5525798eebad3b740e03425
SARojk1234 <sarojking720@gmail.com>
2024-04-03 12:49:21 +0530

    complete search ui page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,32 +1,38 @@
-import React from 'react'
-import { BrowserRouter,Route, Routes } from 'react-router-dom'
-import Header from './component/Header'
-import Home from './pages/Home'
-import About from './pages/About'
-import Profile from './pages/Profile'
-import SignIn from './pages/SignIn'
-import SignUp from './pages/SignUp'
-import PrivateRoute from './component/PrivateRoute'
-import CreateListing from './pages/CreateListing'
-import UpdateListing from './pages/UpdateListing'
-import Listing from './pages/Listing'
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./component/Header";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import PrivateRoute from "./component/PrivateRoute";
+import CreateListing from "./pages/CreateListing";
+import UpdateListing from "./pages/UpdateListing";
+import Listing from "./pages/Listing";
+import Search from "./pages/Search";
 
 export default function App() {
-  return <BrowserRouter>
-  <Header />
-  <Routes>
-    <Route path='/' element={<Home />}/>
-    <Route path='/about' element={<About />}/>
-    <Route element={<PrivateRoute />}>
-    <Route path='/profile' element={<Profile />}/>
-    <Route path='/create-listing' element={<CreateListing />}/>
-    <Route path='/update-listing/:listingId' element={<UpdateListing />}/>
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/create-listing" element={<CreateListing />} />
+          <Route
+            path="/update-listing/:listingId"
+            element={<UpdateListing />}
+          />
+        </Route>
+        <Route path="/listing/:listingId" element={<Listing />} />
 
-    </Route>
-    <Route path='/listing/:listingId' element={<Listing />}/>
-    
-    <Route path='/signin' element={<SignIn />}/>
-    <Route path='/signup' element={<SignUp />} />
-  </Routes>
-  </BrowserRouter>
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/search" element={<Search />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
