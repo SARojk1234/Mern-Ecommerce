da6a93e2e685c08185dd40c0de6cb5c668b19f00
SARojk1234 <sarojking720@gmail.com>
2024-04-12 12:58:51 +0530

    changes for to deployment

--- api/index.js
+++ api/index.js
@@ -5,12 +5,16 @@ import userRoutes from './route/user.route.js'
 import authRoutes from './route/auth.route.js'
 import cookieParser from 'cookie-parser';
 import listingRoutes from './route/listing.route.js'
+import path from 'path';
 dotenv.config();
 
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser())
+
+
+const __dirname=path.resolve();
 
 app.listen(3000,()=>{
     console.log('Server is running on port 3000');
@@ -26,6 +30,12 @@ mongoose.connect(process.env.MONGO).then(()=>{
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/listing', listingRoutes)
+
+app.use(express.static(path.join(__dirname,'/client/dist')));
+
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'))
+})
 
 
 app.use((err,req,res,next)=>{
